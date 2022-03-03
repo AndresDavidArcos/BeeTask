54dcb1be6b6fa0c45a1ff3a8c878386a98274c35
jhan-ai-s13 <jhan.perez@correounivalle.edu.co>
2022-03-02 22:21:33 -0500

    calculadora

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Ponderado from "./components/Ponderado";
 import Home from "./pages/Home";
 function App(){
 
@@ -12,6 +13,8 @@ function App(){
           <Route path='/'element={<Home/>} />
           <Route path='/register'element={<Register/>} />
           <Route path='/login'element={<Login/>} />
+          <Route path='/finalGrade/'element={<Ponderado/>} />
+          
 
         </Routes>
       </Router>
